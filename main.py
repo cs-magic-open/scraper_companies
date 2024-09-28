6e923e62a0f55b689a8cbb65063ee7bd9b688893
markshawn2020 <shawninjuly@gmail.com>
2024-09-28 16:36:44 +0800

    init

---
+++ main.py
@@ -0,0 +1,5 @@
+from selenium import webdriver
+
+if __name__ == '__main__':
+    driver = webdriver.get_default_driver()
+    driver.get("https://www.qcc.com/firm/85d0292125761b813b2408a8138f51ca.html")
